6a58c79f0868aa1c4968ba3c454dbde9ac072bf3
91907 <91907@LAPTOP-VQU24FJE>
2021-07-23 15:39:39 +0530

    first commit

---
+++ src/casetoolsExam/Fruit_rbca19108.java
@@ -0,0 +1,13 @@
+package caseToolsExam;
+   
+public class Fruit_rbca19108 {
+     
+	String color;
+	 void Fruit(String color)
+	{
+	this.color=color;
+	}
+public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	}
+	}
